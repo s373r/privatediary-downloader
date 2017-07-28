5c1d8fa398e71f6a942a59ee53683befe5afc657
s373r <s373r@users.noreply.github.com>
2017-07-28 14:41:14 +0300

    extracted page-models added utils changed project hierarchy

---
+++ src/page-models.js
@@ -0,0 +1,55 @@
+/**
+ * Created by s373r on 26/07/17.
+ */
+
+'use strict';
+
+import { Selector } from 'testcafe';
+
+import { parseEntriesStatus, parseNote } from './utils';
+
+export class LoginPage {
+    constructor () {
+        this.userNameInput = Selector('#UserName');
+        this.passwordInput = Selector('#Password');
+        this.loginButton   = Selector('input[type=submit]');
+    }
+}
+
+export class MainPage {
+    constructor () {
+        this.entriesSummaryText = Selector('body >div.container.body-content > div:nth-child(2) > div.col-md-9');
+        this.readMoreButtons = Selector('a').filter(node => node.textContent === 'Read more ');
+    }
+
+    async getEntriesStatus() {
+        const rawEntriesSummaryText = await this.entriesSummaryText.textContent;
+
+        return parseEntriesStatus(rawEntriesSummaryText);
+    }
+
+    async hrefForEachReadMoreButtons(callback) {
+        const readMoreButtonsCount = await this.readMoreButtons.count;
+
+        for (let i = 0; i < readMoreButtonsCount; ++i) {
+            const readMoreButton = this.readMoreButtons.nth(i);
+            const href           = await readMoreButton.getAttribute('href');
+
+            callback(href);
+        }
+    }
+}
+
+export class NotePage {
+    constructor() {
+        // Because of !@#ing "Uncaught ReferenceError: jQuery is not defined"
+        // on "http://privatediary.net/Records/Edit/", therefore we have the hard way :(
+        this.noteText = Selector('body > div.container.body-content > div:nth-child(2)');
+    }
+
+    async getNote() {
+        const rawNoteText = await this.noteText.innerText;
+
+        return parseNote(rawNoteText);
+    }
+}
---
+++ src/utils.js
@@ -0,0 +1,26 @@
+/**
+ * Created by s373r on 26/07/17.
+ */
+
+'use strict';
+
+function parseEntriesStatus(rawEntriesSummaryText) {
+    const regex     = /Total entries: (\d+) \| Page: 1 of (\d+)/g;
+    const match     = regex.exec(rawEntriesSummaryText);
+    const noteCount = match[1];
+    const pageCount = match[2];
+
+    return [noteCount, pageCount];
+}
+
+function parseNote(rawNoteText) {
+    const noteLines = rawNoteText.split('\n');
+
+    const title     = noteLines[0];
+    const timeStamp = noteLines[noteLines.length - 2].trim();
+    const text      = noteLines.slice(2, noteLines.length - 2).join('\n');
+
+    return { title:title, timeStamp:timeStamp, text:text };
+}
+
+export { parseEntriesStatus, parseNote };
