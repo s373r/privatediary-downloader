fb245484b0fe8f54b4eb691f57d9fa11c993cdc8
Dima Pristupa <dmitry.pristupa@pm.me>
2021-08-29 17:29:55 +0300

    🎨 Remove old fashion C-like code paddings

--- src/utils.js
+++ src/utils.js
@@ -5,8 +5,8 @@
 'use strict';
 
 function parseEntriesStatus(rawEntriesSummaryText) {
-    const regex     = /Total entries: (\d+) \| Page: 1 of (\d+)/g;
-    const match     = regex.exec(rawEntriesSummaryText);
+    const regex = /Total entries: (\d+) \| Page: 1 of (\d+)/g;
+    const match = regex.exec(rawEntriesSummaryText);
     const noteCount = match[1];
     const pageCount = match[2];
 
@@ -15,10 +15,9 @@ function parseEntriesStatus(rawEntriesSummaryText) {
 
 function parseNote(rawNoteText) {
     const noteLines = rawNoteText.split('\n');
-
-    const title     = noteLines[0];
+    const title = noteLines[0];
     const timeStamp = noteLines[noteLines.length - 2].trim();
-    const text      = noteLines.slice(2, noteLines.length - 2).join('\n');
+    const text = noteLines.slice(2, noteLines.length - 2).join('\n');
 
     return { title:title, timeStamp:timeStamp, text:text };
 }
